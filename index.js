b72242f42627ad391ce76ebcab24cbb3d47158d0
Valtteri Laine <valtteri.laine@vr.fi>
2021-06-03 10:58:51 +0300

    Changing API

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ const https = require("https");
 
 const options = {
   method: "GET",
-  hostname: "rest.coinapi.io",
-  path: "/v1/exchangerate/",
+  hostname: "pro-api.coinmarketcap.com",
+  path: "/v1/cryptocurrency/quotes/latest",
   headers: {
-    "X-CoinAPI-Key": process.env.COIN_API_KEY,
+    "X-CMC_PRO_API_KEY": process.env.CMC_API_KEY,
     "content-type": "application/json",
+    Accept: "application/json",
   },
+  json: true,
+  gzip: true,
 };
 
-const getPath = (shortName) => options.path + shortName + "/USD";
-
-const fetchCrypto = (crypto) => {
+const fetchCryptos = (ids) => {
   return new Promise((resolve, reject) => {
     https
-      .get({ ...options, path: getPath(crypto.shortName) }, (res) => {
-        res.on("data", (d) => {
-          resolve({ ...JSON.parse(d.toString()), ...crypto });
+      .get({ ...options, path: options.path + "?id=" + ids }, (res) => {
+        const data = [];
+        res.on("data", (chunk) => {
+          data.push(chunk);
+        });
+        res.on("end", () => {
+          const res = JSON.parse(Buffer.concat(data).toString());
+          resolve(res);
         });
       })
       .on("error", (e) => {
@@ -42,7 +48,6 @@ const sendEmail = async (subject, text) => {
       subject: subject,
       text: text,
     });
-
     console.log("Email sent at", new Date());
   } catch (err) {
     console.error("Failed to send email!", err);
@@ -59,42 +64,56 @@ const cryptos = [
     shortName: "ADA",
     highTreshhold: 2.2,
     lowThreshold: 1.3,
+    id: 2010,
   },
   {
     name: "Polka Dot",
     shortName: "DOT",
     highTreshhold: 45,
     lowThreshold: 17.85,
+    id: 6636,
   },
   {
     name: "Bitcoin",
     shortName: "BTC",
     highTreshhold: 60000,
     lowThreshold: 34500,
+    id: 1,
   },
   {
     name: "Ethereum",
     shortName: "ETH",
     highTreshhold: 3700,
     lowThreshold: 2000,
+    id: 1027,
   },
   {
     name: "XRP",
     shortName: "XRP",
     highTreshhold: 1.6,
     lowThreshold: 0.6,
+    id: 52,
   },
   {
     name: "Stellar",
     shortName: "XLM",
     highTreshhold: 0.69,
     lowThreshold: 0.37,
+    id: 512,
   },
   {
     name: "Tron",
     shortName: "TRX",
     highTreshhold: 0.15,
     lowThreshold: 0.07,
+    id: 1958,
+  },
+  {
+    name: "Ankr",
+    shortName: "ANKR",
+    highTreshhold: 0.16,
+    lowThreshold: 0.07,
+    id: 3783,
   },
 ];
 
@@ -121,37 +140,65 @@ const sendHighAlarmEmails = async (alarmingCryptos) => {
 };
 
 const fetchPrices = async () => {
-  const promises = cryptos.map((crypto) => fetchCrypto(crypto));
-
-  return Promise.all(promises);
+  return fetchCryptos(cryptos.map((crypto) => crypto.id).join(","));
 };
 
 const filterByCryptosOverPriceRange = (cryptosWithPrices) => {
   return cryptosWithPrices.filter(
-    (crypto) => crypto.rate > crypto.highTreshhold
+    (crypto) => crypto.quote.USD.price > crypto.highTreshhold
   );
 };
 
 const filterByCryptosUnderPriceRange = (cryptosWithPrices) => {
   return cryptosWithPrices.filter(
-    (crypto) => crypto.rate < crypto.lowThreshold
+    (crypto) => crypto.quote.USD.price < crypto.lowThreshold
   );
 };
 
-const alarmLogic = async () => {
-  let cryptosWithPrices;
-  try {
-    cryptosWithPrices = await fetchPrices();
-  } catch (err) {
-    console.error("Fetching prices failed!", err);
-  }
+const enrichCryptosWithLimits = (cryptos, prices) => {
+  return cryptos.map((crypto) => {
+    if (prices && prices.data && prices.data[crypto.id]) {
+      return {
+        ...crypto,
+        ...prices.data[crypto.id],
+      };
+    } else {
+      return crypto;
+    }
+  });
+};
 
-  if (!cryptosWithPrices) {
+const alarmLogic = async () => {
+  let prices = [];
+  try {
+    prices = await fetchPrices();
+  } catch (err) {
+    sendEmail("Failed to fetch crypto prices catch error!", "-");
+    console.error("Fetching prices failed!", err);
     return;
   }
 
-  const highAlarms = filterByCryptosOverPriceRange(cryptosWithPrices) || [];
-  const lowAlarms = filterByCryptosUnderPriceRange(cryptosWithPrices) || [];
+  let enrichedCryptos = enrichCryptosWithLimits(cryptos, prices);
+
+  enrichedCryptos = enrichedCryptos.filter(
+    (crypto) =>
+      crypto && crypto.quote && crypto.quote.USD.price && crypto.quote.USD.price
+  );
+
+  if (!enrichedCryptos || (enrichedCryptos && enrichedCryptos.length === 0)) {
+    sendEmail("Failed to fetch any crypto prices!", "-");
+    return;
+  }
+
+  const highAlarms = filterByCryptosOverPriceRange(enrichedCryptos) || [];
+  const lowAlarms = filterByCryptosUnderPriceRange(enrichedCryptos) || [];
+
+  if (highAlarms.length === 0 && lowAlarms.length === 0) {
+    sendEmail(
+      "Nothing to report :) ",
+      "Fetched this many cryptos succesfully... " + enrichedCryptos.length
+    );
+  }
 
   try {
     await sendHighAlarmEmails(highAlarms);
@@ -174,5 +221,3 @@ exports.handler = () => {
   };
   return response;
 };
-
-alarmLogic();
